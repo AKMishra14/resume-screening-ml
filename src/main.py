caee5a55586dbb69ea0a58c7801636d532e8d29c
AKMishra14 <124565611+AKMishra14@users.noreply.github.com>
2026-01-13 16:48:58 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Resume Screening ML Project")
+from recommender import recommend_role
+
+resume_text = """
+Python machine learning data analysis pandas numpy
+"""
+
+recommended_role = recommend_role(resume_text)
+
+print("Recommended Job Role:")
+print(recommended_role)
